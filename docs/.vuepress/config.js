4b814904ba96fa34553e5b0095915248b09f2ccc
TadpoleStack <istack@yeah.net>
2019-04-11 21:16:12 +0800

    dev-stack-create

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    title: 'Tadpole——技术栈',
+    description: '这里是Tadpole的技术栈',
+    head: [
+        ['link', { rel: 'icon', href: '/icon.png' }]
+      ],
+      dest: './dist',
+      port: '8080',
+      markdown: {
+          lineNumbers: true
+      },
+
+     /**
+     * 主题设置
+     */
+    themeConfig: {
+        nav: require('./nav'),//nav部分模块化
+        sidebar: require('./sidebar'),//sidebar部分模块化
+        sidebarDepth: 2,
+        lastUpdated: 'Last Updated',
+        searchMaxSuggestoins: 10,
+        serviceWorker: {
+            updatePopup: {
+                message: "New content is available.",
+                buttonText: 'Refresh'
+            }
+        },
+        editLinks: true,
+        editLinkText: '在 GitHub 上编辑此页 ！'
+    }
+  }
---
+++ docs/.vuepress/nav.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        text: '首页', link: '/'
+    },
+    {
+        text: '学习资源',
+        items: [
+            {text: '徐老师的技术博客', link: 'http://www.100phone.cn'},
+            {text: '田老师的技术博客', link: 'https://me.csdn.net/jiang7701037'}
+        ]
+    },
+    {
+        text: '个人主页',
+        items: [
+            {
+                text: '技术简介', link: '/'
+            }
+        ]
+    },
+    {
+        text: '博客',link: 'https://blog.mewebsite.cn'
+    },
+    {
+        text: 'GitHub',link:'https://github.com/TadpoleStack'
+    }
+]
