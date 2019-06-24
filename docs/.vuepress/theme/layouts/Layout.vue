e576d75d314b6834b1684f66b3d5816c76c0f97f
TadpoleStack <istack@yeah.net>
2019-06-24 22:16:10 +0800

    访问父主题初探主题slot修改layout.vue——主题布局

---
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,24 @@
+<template>
+  <ParentLayout>
+    <div slot="sidebar-top">this is sidebar-top slot</div>
+    <div slot="sidebar-bottom">this is sidebar-bottom slot</div>
+    <div slot="page-top" class="page-top">this is page-top slot</div>
+    <div slot="page-bottom">this is page-bottom slot</div>
+  </ParentLayout>
+</template>
+
+<script>
+  import ParentLayout from '@parent-theme/layouts/Layout.vue'
+export default {
+  components: {
+    ParentLayout
+  }
+}
+</script>
+
+<style lang="stylus">
+  .page-top{
+    margin-top:3.6rem;
+  }
+</style>
+
