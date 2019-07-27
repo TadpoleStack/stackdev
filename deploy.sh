725c4578f86c1a50571b7d146f55719e2510b8f9
TadpoleStack <istack@yean.net>
2019-07-27 21:49:14 +0800

    部署coding和码云

--- deploy.sh
+++ deploy.sh
@@ -10,16 +10,19 @@ npm run build
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-#echo 'istack.site' > CNAME
- echo 'istack.site' > CNAME
+echo 'istack.site' > CNAME
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
- git push -f git@github.com:TadpoleStack/TadpoleStack.github.io.git master
+git push -f git@github.com:TadpoleStack/TadpoleStack.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
+# 腾讯云开发者平台（coding） 
+# git push -f https://git.dev.tencent.com/mrchen1521620063/mrchen1521620063.git master
+# 码云
+# git push -f https://gitee.com/TadpoleStack/TadpoleStack.git master
 cd -
